e6b1da3e2c019df3cf0764732675e57f0a0cebd6
Han Hoang <katiehoang268@gmail.com>
2022-08-26 02:10:29 -0500

    Changes to be committed:

--- creds.js
+++ creds.js
@@ -1,11 +1,19 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//   host: "3380db.cs.uh.edu",
+//   user: "dbs042",
+//   password: "2088807H",
+//   database: "COSC3380",
+//   port: 5432
+// });
 const pool = new Pool({
-  host: "3380db.cs.uh.edu",
-  user: "dbs042",
-  password: "2088807H",
-  database: "COSC3380",
+  host: "kashin.db.elephantsql.com",
+  user: "onkwqgmw",
+  password: "a6PHKnm_J7dhj58jKcXvyc06cKR3seMF",
+  database: "onkwqgmw",
   port: 5432
 });
+
 
 module.exports = pool;
---
+++ query.sql
@@ -0,0 +1,81 @@
+SELECT * FROM flight
+SELECT * FROM boarding
+SELECT * FROM flight
+SELECT * FROM flight
+SELECT * FROM boarding
+SELECT * FROM airplane
+SELECT * FROM flight
+SELECT * FROM flight
+SELECT * FROM boarding
+SELECT * FROM airplane
+SELECT * FROM boarding
+SELECT * FROM flight
+SELECT * FROM flight
+SELECT * FROM boarding
+SELECT * FROM airplane
+SELECT * FROM boarding
+SELECT * FROM gate WHERE gate_no = B7
+SELECT * FROM boarding
+SELECT * FROM gate WHERE gate_no = B7
+SELECT * FROM boarding
+SELECT * FROM gate WHERE gate_no = A2
+SELECT * FROM airplane
+SELECT * FROM maintenance WHERE maintenance_id = 3
+SELECT * FROM airplane
+SELECT * FROM airplane
+SELECT * FROM boarding
+SELECT * FROM flight
+SELECT * FROM flight WHERE flight_id = US101
+SELECT * FROM flight
+SELECT * FROM airplane
+SELECT * FROM boarding
+SELECT * FROM airplane
+SELECT * FROM service WHERE service_id = 10
+SELECT * FROM airplane
+SELECT * FROM maintenance WHERE maintenance_id = 5
+SELECT * FROM airplane
+SELECT * FROM maintenance WHERE maintenance_id = 6
+SELECT * FROM airplane
+SELECT * FROM boarding
+SELECT * FROM flight
+SELECT * FROM airplane
+SELECT * FROM boarding
+SELECT * FROM gate WHERE gate_no = B7
+SELECT * FROM boarding
+SELECT * FROM gate WHERE gate_no = B7
+SELECT * FROM boarding
+SELECT * FROM gate WHERE gate_no = B7
+SELECT * FROM boarding
+SELECT * FROM boarding
+SELECT * FROM boarding
+SELECT * FROM flight
+SELECT * FROM boarding
+SELECT * FROM airplane
+SELECT * FROM flight
+SELECT * FROM boarding
+SELECT * FROM flight
+SELECT * FROM flight
+SELECT * FROM boarding
+SELECT * FROM boarding
+SELECT * FROM flight
+SELECT * FROM boarding
+SELECT * FROM flight
+SELECT * FROM flight
+SELECT * FROM boarding
+SELECT * FROM flight
+SELECT * FROM flight
+SELECT * FROM boarding
+SELECT * FROM airplane
+SELECT * FROM flight
+SELECT * FROM airplane
+SELECT * FROM boarding
+SELECT * FROM flight
+SELECT * FROM boarding
+SELECT * FROM flight
+SELECT * FROM flight
+SELECT * FROM flight
+SELECT * FROM flight
+SELECT * FROM flight
+SELECT * FROM boarding
+SELECT * FROM airplane
+SELECT * FROM flight
